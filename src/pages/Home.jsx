2612b650c060a98c1941dd6da62fdb75877e2d0d
Baptiste71 <planchonbaptiste@gmail.com>
2022-04-20 12:37:30 +0200

    mise en place home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
 const Home = () => {
   return (
     <div className="home">
-      <img className="logo_groupomania" src="..\public\img\icon-above-font.webp" alt="logo Groupomania" />
-
-      <p>Partager votre expérience au sein de Groupomania !</p>
-      <div className="connection">
-        <form className="form-home" action="./Login.jsx">
-          <button className="btn" type="submit" name="Connexion">
-            Connexion
-          </button>
-        </form>
+      <div class="background_groupomania">
+        <img class="logo_groupomania" src="img\icon-above-font.webp" alt="logo Groupomania" />
+      </div>
+      <div class="two_parts">
+        <div class="presentation">
+          <p>Partagez votre expérience au sein de Groupmania !</p>
+        </div>
+        <div class="connection">
+          <form class="form-home" action="pages_groupomania\Login.html">
+            <button class="btn" type="submit" name="Connexion">
+              Connexion
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
