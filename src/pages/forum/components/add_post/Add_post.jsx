5b296a14f2597f72e4dca12f273e2acf479a7578
Baptiste71 <planchonbaptiste@gmail.com>
2022-05-02 11:33:05 +0200

    création des routes post 1

--- src/pages/forum/components/add_post/Add_post.jsx
+++ src/pages/forum/components/add_post/Add_post.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./_add_post.scss";
 import PersonIcon from "@material-ui/icons/Person";
 import AddPhotoAlternateIcon from "@material-ui/icons/AddPhotoAlternate";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import { useNavigate } from "react-router-dom";
 
 const Add_post = () => {
+  const [msg, setMsg] = useState("");
+  const [files, setFiles] = useState("");
+  const navigate = useNavigate();
+
+  const post = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000", {
+        file: files,
+        message: msg,
+      });
+      navigate.push("/forum");
+    } catch (error) {
+      if (error.response) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div className="add_post">
       <div className="title">
@@ -20,14 +41,19 @@ const Add_post = () => {
       </div>
       <div className="create_post">
         <div className="add_files">
-          <AddPhotoAlternateIcon className="icon_photo" />
+          <button className="add_image">
+            <AddPhotoAlternateIcon className="icon_photo" />
+          </button>
+          <input type="file" accept="image/png, image/jpeg, image/webp" name="add_image" className="input_image" onChange={(e) => setFiles(e.target.value)} />
         </div>
         <div className="msg_post">
-          <form className="msg" /*onSubmit={createUser}*/>
+          <form className="msg">
             <div className="input_arrow">
-              <input className="message" type="text" required onChange={(e) => /*setFirstname*/ e.target.value} />
-              <div className="arrow_send" /*onSubmit={*/>
-                <ChevronRightIcon className="sent_msg" />
+              <input className="message" type="text" />
+              <div className="arrow_send" onSubmit={post}>
+                <button className="add_txt" onChange={(e) => setMsg(e.target.value)}>
+                  <ChevronRightIcon className="sent_msg" />
+                </button>
               </div>
             </div>
           </form>
--- src/pages/forum/components/all posts/All_posts.jsx
+++ src/pages/forum/components/all posts/All_posts.jsx
@@ -1,17 +1,51 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./_all_posts.scss";
 
 const All_posts = () => {
+  const [msg, setMsg] = useState("");
+  const [files, setFiles] = useState("");
+
+  const everything = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/posts", {
+        file: files,
+        message: msg,
+      });
+    } catch (error) {
+      if (error.response) {
+        console.log(error);
+      }
+    }
+  };
+
+  const onePost = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/posts/:id", {
+        file: files,
+        message: msg,
+      });
+    } catch (error) {
+      if (error.response) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div className="last_post">
       <div className="title">
         <h1>All posts</h1>
       </div>
       <div className="see_all">
-        <a href="#">See all</a>
+        <button className="see_all_post">
+          <a onSubmit={everything}>See all</a>
+        </button>
       </div>
       <div className="card_post">
-        <a href="#">
+        <a onSubmit={onePost}>
           <div className="img_post">
             <img src="img\grouponamia_rognee.png" alt="Logo groupomania" />
           </div>
